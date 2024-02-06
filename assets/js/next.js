6ac69099aec789bf352e4bf5cbe7f55b768bb8e7
AatiqR <aatiq9527@gmail.com>
2024-02-06 23:02:20 +0500

    MasterClass

---
+++ assets/js/next.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * add event on element
+ */
+const addEventOnElem = function (elem, type, callback) {
+  if (elem instanceof NodeList) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+/**
+ * navbar toggle
+ */
+const navbar = document.querySelector("[data-navbaz]");
+const navToggler = document.querySelectorAll("[data-nav-toggler]");
+const navLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+};
+
+addEventOnElem(navToggler, "click", toggleNavbar);
